ff36e716e86068d4896c26cc034c83da9e631831
m8r1x <m8r1x.io@gmail.com>
2017-07-10 20:35:39 +0300

    added access to api with access tokens

--- src/js/app/social/SocialSaga.js
+++ src/js/app/social/SocialSaga.js
@@ -8,10 +8,13 @@ import {
   FETCH_SOCIAL_REJECTED 
 } from './SocialActions'
 
+// $.defaults.baseURL = 'https://id3ntityapi.herokuapp.com/users'
+$.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0OTk3MDgwMjR9.nRyldVw89WtcYU9zDUsMyINmmYDmUEZ2U6gPv4pmUks'
+
 function* fetchSocial() {
   try {
-    const response = yield call($.get, 'http://localhost:3000/users')
-    const social = response.data[0].social
+    const response = yield call($.get, 'http://id3ntityapi.herokuapp.com/users')
+    const social = response.data[0].socials
     yield put({ type: FETCH_SOCIAL_FULFILLED, payload: social })
   } catch (e) {
     yield put({ type: FETCH_SOCIAL_REJECTED, payload: e.message })
