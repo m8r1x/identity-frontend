3187794b9c12c0f6e1c3653bca7b9a82a0eb4afb
m8r1x <m8r1x.io@gmail.com>
2017-07-12 14:22:34 +0300

    added a home page route

--- src/js/client.js
+++ src/js/client.js
@@ -7,6 +7,7 @@ import store from './store'
 
 import App from './app/App'
 import CardContainer from './app/card/CardContainer'
+import Index from './app/Index'
 import NoMatch from './noMatch/NoMatch'
 import SignUp from './signup/SignUp'
 
@@ -15,7 +16,8 @@ render(
     <BrowserRouter>
       <App>
         <Switch>
-          <Route exact path="/" component={CardContainer} />
+          <Route exact path="/" component={Index} />
+          <Route exact path="/home" component={CardContainer} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/404" component={NoMatch} />
         </Switch>
