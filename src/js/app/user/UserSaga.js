7186c687f0e0c2db7f340420f338f1175148d5af
Willie M.I.K <m8r1x.io@gmail.com>
2017-07-28 13:27:54 +0300

    graphql api test

--- src/js/app/user/UserSaga.js
+++ src/js/app/user/UserSaga.js
@@ -9,14 +9,14 @@ import {
 } from './UserActions'
 
 // $.defaults.baseURL = 'https://id3ntityapi.herokuapp.com/users'
-$.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0OTk3MDgwMjR9.nRyldVw89WtcYU9zDUsMyINmmYDmUEZ2U6gPv4pmUks'
-
+// $.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0OTk3MDgwMjR9.nRyldVw89WtcYU9zDUsMyINmmYDmUEZ2U6gPv4pmUks'
+url = 'http://identitygql.herokuapp.com/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjU5NzI0YzcwMWE3YjJlMDk4MDJkYjRkMyJ9LCJpYXQiOjE1MDA2NjI5NjgsImV4cCI6MTUzMjIyMDU2OH0.2kL5VctdRQSJoAyM3esX6K6kF3yBKPphbp1ZOXS87eI&query={users(limit: 2){name career social{github linkedin twitter}}}'
 function* fetchUser() {
   try {
-    const response = yield call($.get, 'https://id3ntityapi.herokuapp.com/users')
+    const response = yield call($.get, url/*'https://id3ntityapi.herokuapp.com/users'*/)
     const user = {
-      name: response.data[0].name, 
-      career: response.data[0].career
+      name: response.data.users[0].name, 
+      career: response.data.users[0].career
     }
     yield put({ type: FETCH_USER_FULFILLED, payload: user })
 
