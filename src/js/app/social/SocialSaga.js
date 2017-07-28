8121d8218c67fd8bd3517652516b642ce2b47c56
m8r1x <m8r1x.io@gmail.com>
2017-07-28 13:50:23 +0300

    reverted to RESTful API from GraphQL test

--- src/js/app/social/SocialSaga.js
+++ src/js/app/social/SocialSaga.js
@@ -9,12 +9,12 @@ import {
 } from './SocialActions'
 
 // $.defaults.baseURL = 'https://id3ntityapi.herokuapp.com/users'
-//$.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0OTk3MDgwMjR9.nRyldVw89WtcYU9zDUsMyINmmYDmUEZ2U6gPv4pmUks'
-url = 'http://identitygql.herokuapp.com/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjU5NzI0YzcwMWE3YjJlMDk4MDJkYjRkMyJ9LCJpYXQiOjE1MDA2NjI5NjgsImV4cCI6MTUzMjIyMDU2OH0.2kL5VctdRQSJoAyM3esX6K6kF3yBKPphbp1ZOXS87eI&query={users(limit: 2){name career social{github linkedin twitter}}}'
+$.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0OTk3MDgwMjR9.nRyldVw89WtcYU9zDUsMyINmmYDmUEZ2U6gPv4pmUks'
+// const url = 'http://identitygql.herokuapp.com/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjU5NzI0YzcwMWE3YjJlMDk4MDJkYjRkMyJ9LCJpYXQiOjE1MDA2NjI5NjgsImV4cCI6MTUzMjIyMDU2OH0.2kL5VctdRQSJoAyM3esX6K6kF3yBKPphbp1ZOXS87eI&query={users(limit: 2){name career social{github linkedin twitter}}}'
 function* fetchSocial() {
   try {
-    const response = yield call($.get, url/*'https://id3ntityapi.herokuapp.com/users'*/)
-    const social = response.data.users[0].social
+    const response = yield call($.get, /*url*/'https://id3ntityapi.herokuapp.com/users')
+    const social = response.data[0].social
     yield put({ type: FETCH_SOCIAL_FULFILLED, payload: social })
   } catch (e) {
     yield put({ type: FETCH_SOCIAL_REJECTED, payload: e.message })
